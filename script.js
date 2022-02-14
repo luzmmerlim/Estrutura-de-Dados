034ae3c97b4539cb527b0c60b4957335b8f48efd
luzmmerlim <luziamerlim.1309@gmail.com>
2022-02-14 16:36:18 -0300

    exercicio estrutura de dados

---
+++ script.js
@@ -0,0 +1,31 @@
+// Crie um array que receba 5 itens e exiba no console.
+
+let nomes =["Monica","Chandler","Rachel","Joey","Phoebe"];
+console.log(nomes);
+
+//Utilize um método para adicionar um nome ao inicio do array.
+
+nomes.unshift("Ross");
+console.log(nomes);
+
+// Utilize um método para remover o último nome do array.
+
+nomes.pop();
+console.log(nomes);
+
+// Utilize um método para adicionar dois nomes ao fim do array.
+
+nomes.push("Emma","Ben")
+console.log(nomes);
+
+// Utilize um método para remover o primeiro nome do array.
+
+nomes.shift();
+console.log(nomes);
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+// const numbers = [7,5,6,3,8,9,2,1,4]
+
+ const numbers = [7,5,6,3,8,9,2,1,4]
+ numbers.sort();
+ console.log(numbers);
